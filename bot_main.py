f2db9e812f17e259dac8f5bbc9bddd66f4bc25e4
Goryaeva Kseniya <goryaevakss@gmail.com>
2026-03-27 12:08:12 +0300

    [BOT-1] Добавлено подключение к боту, обработчик команд start и help.

--- bot_main.py
+++ bot_main.py
@@ -1,3 +1,5 @@
+from math import lgamma
+
 from telebot import TeleBot
 from config import StandConfig
 
@@ -10,7 +12,16 @@ bot = TeleBot(token=bot_token)
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
     print(message)
-    bot.reply_to(message, "Answer")
+    bot.reply_to(message, "обработчик команд start и help")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    print(message)
+    bot.reply_to(message, f"Hi, {message.chat.first_name}, your message= {message.text}"
+                          f"your username = {message.chat.username}")
+
+
 
 bot.infinity_polling()  # запуск бота и прием сообщений
 # пока не работает из-за впн
