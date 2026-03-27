9907941cc04e82c0cfbc7bed5d55b4474ba4d410
Goryaeva Kseniya <goryaevakss@gmail.com>
2026-03-27 14:12:58 +0300

    [BOT-1] Добавлены модельки и обработчик команд с кнопками и состоянием.

--- bot_main.py
+++ bot_main.py
@@ -1,27 +1,33 @@
-from math import lgamma
-
-from telebot import TeleBot
+from telebot import TeleBot, types
 from config import StandConfig
-
+from telebot.types import Message
+from models.bot_model import BotMessageResponse
 
 config = StandConfig()
 bot_token = config.bot_token
 bot = TeleBot(token=bot_token)
 
-# обработчик команд start и help с декоратором message_handler
-@bot.message_handler(commands=["start", "help"])
-def send_welcome(message):
-    print(message)
-    bot.reply_to(message, "обработчик команд start и help")
+
+# эхо любого сообщения
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message: Message) -> BotMessageResponse:
+#     print(message)
+#     response_text = (
+#         f"Hi, {message.from_user.first_name}!\n"
+#         f"Your message: {message.text}\n"
+#         f"Your username: @{message.from_user.username if message.from_user.username else 'not set'}\n"
+#         f"Your telegram_id: {message.from_user.id}\n"
+#         f"{"You are bot" if message.from_user.is_bot else "You are not bot"}\n"
+#         f"{"Your lastname:" if message.from_user.last_name else "You have not lastname"}\n"
+#         f"{"Your title:" if message.chat.title else "You have not title"}"
+#     )
+#     bot.reply_to(message, response_text)
+#     response_model = BotMessageResponse()
+#     return response_model
 
 
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    print(message)
-    bot.reply_to(message, f"Hi, {message.chat.first_name}, your message= {message.text}"
-                          f"your username = {message.chat.username}")
 
 
 
-bot.infinity_polling()  # запуск бота и прием сообщений
-# пока не работает из-за впн
+
+# bot.infinity_polling()  # запуск бота и прием сообщений
